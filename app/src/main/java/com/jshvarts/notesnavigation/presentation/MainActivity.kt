5bfe96fb8c05b46b273d44643c95b02445e75d8f
James Shvarts <jshvarts@gmail.com>
2018-12-24 15:48:34 -0500

    Update nav arch component

--- app/src/main/java/com/jshvarts/notesnavigation/presentation/MainActivity.kt
+++ app/src/main/java/com/jshvarts/notesnavigation/presentation/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.navigation.Navigation.findNavController
-import androidx.navigation.ui.NavigationUI.*
+import androidx.navigation.ui.NavigationUI.navigateUp
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
+import androidx.navigation.ui.NavigationUI.setupWithNavController
 import com.jshvarts.notesnavigation.R
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -19,7 +21,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return navigateUp(drawerLayout, findNavController(this, R.id.nav_host_fragment))
+        return navigateUp(findNavController(this, R.id.nav_host_fragment), drawerLayout)
     }
 
     override fun onBackPressed() {
